aa1aa9cf58b981905646a327965f9818da740b32
therajeshyadav <140382326+therajeshyadav@users.noreply.github.com>
2025-09-09 23:50:33 +0530

    Update leadController.js

--- Backend/controllers/leadController.js
+++ Backend/controllers/leadController.js
@@ -1,25 +1,55 @@
+import Customer from "../models/Customer.js";
 import Lead from "../models/Lead.js";
 import mongoose from "mongoose";
+
 export const createLead = async (req, res) => {
   try {
-    const lead = await Lead.create(req.body);
+    const { customerId, ...leadData } = req.body;
+
+    // Check customer belongs to logged in user
+    const customer = await Customer.findOne({
+      _id: customerId,
+      ownerId: req.user.id,
+    });
+
+    if (!customer) {
+      return res
+        .status(403)
+        .json({ msg: "Not authorized to add lead for this customer" });
+    }
+
+    const lead = await Lead.create({
+      ...leadData,
+      customerId,
+    });
+
     res.status(201).json(lead);
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
 };
 
-
-
 export const getLeads = async (req, res) => {
   try {
     const { customerId, status, page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+
+    // Ensure this customer belongs to logged in user
+    if (customerId) {
+      const customer = await Customer.findOne({
+        _id: customerId,
+        ownerId: req.user.id,
+      });
+      if (!customer) {
+        return res
+          .status(403)
+          .json({ msg: "Not authorized to view leads for this customer" });
+      }
+    }
 
     const query = {};
-    if (customerId) query.customerId = new mongoose.Types.ObjectId(customerId); // <-- fixed
+    if (customerId) query.customerId = new mongoose.Types.ObjectId(customerId);
     if (status && status !== "all") query.status = status;
-
-    const skip = (page - 1) * limit;
 
     const [leads, totalLeads] = await Promise.all([
       Lead.find(query)
@@ -31,8 +61,6 @@ export const getLeads = async (req, res) => {
 
     const totalPages = Math.ceil(totalLeads / limit);
 
-    console.log("Fetching leads with query:", query, "Found:", leads.length);
-
     res.json({
       leads,
       pagination: {
@@ -42,17 +70,24 @@ export const getLeads = async (req, res) => {
       },
     });
   } catch (err) {
-    console.error(err);
     res.status(500).json({ msg: err.message });
   }
 };
 
-
 export const updateLead = async (req, res) => {
   try {
-    const lead = await Lead.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
+    const lead = await Lead.findById(req.params.id).populate("customerId");
+
+    if (!lead) return res.status(404).json({ msg: "Lead not found" });
+    if (lead.customerId.ownerId.toString() !== req.user.id) {
+      return res
+        .status(403)
+        .json({ msg: "Not authorized to update this lead" });
+    }
+
+    Object.assign(lead, req.body);
+    await lead.save();
+
     res.json(lead);
   } catch (error) {
     res.status(500).json({ msg: error.message });
@@ -61,7 +96,16 @@ export const updateLead = async (req, res) => {
 
 export const deleteLead = async (req, res) => {
   try {
-    await Lead.findByIdAndDelete(req.params.id);
+    const lead = await Lead.findById(req.params.id).populate("customerId");
+
+    if (!lead) return res.status(404).json({ msg: "Lead not found" });
+    if (lead.customerId.ownerId.toString() !== req.user.id) {
+      return res
+        .status(403)
+        .json({ msg: "Not authorized to delete this lead" });
+    }
+
+    await lead.deleteOne();
     res.json({ msg: "Lead deleted" });
   } catch (error) {
     res.status(500).json({ msg: error.message });
@@ -72,12 +116,39 @@ export const getLeadStats = async (req, res) => {
   try {
     // Group by status for count
     const statusDistribution = await Lead.aggregate([
+      {
+        $lookup: {
+          from: "customers", // Customer collection
+          localField: "customerId",
+          foreignField: "_id",
+          as: "customer",
+        },
+      },
+      { $unwind: "$customer" }, // Flatten the array
+      {
+        $match: {
+          "customer.ownerId": new mongoose.Types.ObjectId(req.user.id),
+        },
+      },
       { $group: { _id: "$status", count: { $sum: 1 } } },
       { $project: { status: "$_id", count: 1, _id: 0 } },
     ]);
 
-    // Group by status for total value
     const valueDistribution = await Lead.aggregate([
+      {
+        $lookup: {
+          from: "customers",
+          localField: "customerId",
+          foreignField: "_id",
+          as: "customer",
+        },
+      },
+      { $unwind: "$customer" },
+      {
+        $match: {
+          "customer.ownerId": new mongoose.Types.ObjectId(req.user.id),
+        },
+      },
       { $group: { _id: "$status", totalValue: { $sum: "$value" } } },
       { $project: { status: "$_id", totalValue: 1, _id: 0 } },
     ]);
@@ -88,4 +159,3 @@ export const getLeadStats = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
-
